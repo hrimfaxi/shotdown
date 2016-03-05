d5b02ae7011f50d57dc0491971ec85ffad654a0e
hrimfaxi <outmatch@gmail.com>
2016-03-05 18:59:44 +0800

    added options to output filename and specify subtitle index to download Output-dir renamed as -O
    And a little refactor on download_subtitle
    
    Signed-off-by: hrimfaxi <outmatch@gmail.com>

--- shotdown/main.py
+++ shotdown/main.py
@@ -33,14 +33,16 @@ def get_subtitle(r, va):
         for i in range(len(r)):
             for j in range(len(r[i]["Files"])):
                 srt_url = r[i]["Files"][j]["Link"]
-                log.info ("Downloading subtitle...")
                 log.debug (srt_url)
                 net.download_subtitle(srt_url, dest_dir=va['output_dir'])
     else:
-        srt_url = r[0]["Files"][0]["Link"]
-        log.info ("Downloading subtitle...")
+        srt_url = r[va["index"]]["Files"][0]["Link"]
         log.debug (srt_url)
-        net.download_subtitle(srt_url, dest_dir=va['output_dir'])
+
+        if va['output_filename']:
+            net.download_subtitle(srt_url, path=va['output_filename'], dest_dir=va['output_dir'])
+        else:
+            net.download_subtitle(srt_url, dest_dir=va['output_dir'])
 
 def get_subtitles(va):
     mkdir_recursive(va['output_dir'])
@@ -68,15 +70,20 @@ def main():
     parser.add_argument('-a', '--all', action='store_true', help='Download all subtitles')
     parser.add_argument('-d', '--debug', action='store_true', help='Enable debug mode')
     parser.add_argument('-n', '--no-download', dest='no_download', action='store_true', help='Don\'t download subtitle, show metadata only')
+    parser.add_argument('-i', '--index', type=int, \
+            default=0, help='Choice subtitle index to download, starting from 0')
     parser.add_argument('-l', '--language', type=str, \
             default='', choices=('chn', 'eng'), help='Choice subtitle language')
-    parser.add_argument('-o', '--output-dir', dest='output_dir', type=str, \
+    parser.add_argument('-o', '--output-filename', dest='output_filename', \
+            type=str, help='Set output directory')
+    parser.add_argument('-O', '--output-dir', dest='output_dir', type=str, \
             default=get_subtitle_directory(), help='Set output directory')
     parser.add_argument('--version', action='version', version='%(prog)s ' + __version__)
     parser.add_argument('files', type=str, nargs='+', help='video file paths to search for subtitle')
     args = parser.parse_args()
     va = vars(args)
     log.init_logger('debug' if va['debug'] else 'info')
+    log.debug("va: %s", str(va))
     get_subtitles(va)
 
 if __name__ == "__main__":
--- shotdown/net.py
+++ shotdown/net.py
@@ -42,6 +42,7 @@ def get_metadata(pathinfo, filehash="", lang=""):
     return res_js
 
 def download_subtitle(url, path="", dest_dir="", ):
+    log.info ("Downloading subtitle...")
     req = urllib2.Request(url, '', HEADERS)
     response = urllib2.urlopen(req)
 
